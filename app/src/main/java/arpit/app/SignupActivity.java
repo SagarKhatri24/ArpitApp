0939bd50237df5f43270f52e7d83a2895279dc4e
Sagar Khatri <khatrisagar2@gmail.com>
2025-10-31 17:08:28 +0530

    Signup Spinner

--- app/src/main/java/arpit/app/SignupActivity.java
+++ app/src/main/java/arpit/app/SignupActivity.java
@@ -3,11 +3,18 @@ package arpit.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,11 +22,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class SignupActivity extends AppCompatActivity {
 
     Button signup;
     EditText name,email,contact,password,confirmPassword;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    //RadioButton male,female;
+    RadioGroup gender;
+    CheckBox acceptTerms;
+
+    Spinner city;
+
+    //String[] cityArray = {"Ahmedabad","Vadodara","Surat","Rajkot"};
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +56,66 @@ public class SignupActivity extends AppCompatActivity {
 
         password = findViewById(R.id.signup_password);
         confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        city = findViewById(R.id.signup_city);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("Ahmedabad");
+        arrayList.add("Surat");
+        arrayList.add("Vadodara");
+        arrayList.add("Demo");
+        arrayList.add("Gandhinagar");
+        arrayList.add("Rajkot");
+
+        arrayList.remove(3);
+        arrayList.add(0,"Select City");
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1,arrayList);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        city.setAdapter(adapter);
+
+        city.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i!=0) {
+                    Toast.makeText(SignupActivity.this, arrayList.get(i), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        acceptTerms = findViewById(R.id.signup_terms);
+
+        gender = findViewById(R.id.signup_gender);
+
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup radioGroup, int i) {
+                RadioButton radioButton = findViewById(i);
+                Toast.makeText(SignupActivity.this, radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*male = findViewById(R.id.signup_male);
+        female = findViewById(R.id.signup_female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignupActivity.this, "Male", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignupActivity.this, "Female", Toast.LENGTH_SHORT).show();
+            }
+        });*/
 
         signup = findViewById(R.id.signup_button);
         signup.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +150,12 @@ public class SignupActivity extends AppCompatActivity {
                 }
                 else if(!password.getText().toString().trim().matches(confirmPassword.getText().toString().trim())){
                     confirmPassword.setError("Password Does Not Match");
+                }
+                else if(gender.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(SignupActivity.this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+                }
+                else if(!acceptTerms.isChecked()){
+                    Toast.makeText(SignupActivity.this, "Please Accept Terms & Conditions", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     System.out.println("Signup Successfully");
