8557e158bf1bfa5e6a0e254a3c37b76c8c2410db
Sagar Khatri <khatrisagar2@gmail.com>
2025-11-05 17:16:54 +0530

    Session Management

--- app/src/main/java/arpit/app/MainActivity.java
+++ app/src/main/java/arpit/app/MainActivity.java
@@ -1,9 +1,11 @@
 package arpit.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public static EditText username,password;
     TextView signup;
     SQLiteDatabase db;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sp = getSharedPreferences(ConstantSp.PREF,MODE_PRIVATE);
 
         db = openOrCreateDatabase("Arpit.db",MODE_PRIVATE,null);
 
@@ -61,6 +66,27 @@ public class MainActivity extends AppCompatActivity {
                     String selectQuery = "SELECT * FROM USERS WHERE EMAIL='"+username.getText().toString()+"' AND PASSWORD='"+password.getText().toString()+"'";
                     Cursor cursor = db.rawQuery(selectQuery,null);
                     if(cursor.getCount()>0) {
+                        while (cursor.moveToNext()){
+                            String sUserId = cursor.getString(0);
+                            String sName = cursor.getString(1);
+                            String sEmail = cursor.getString(2);
+                            String sContact = cursor.getString(3);
+                            String sPassword = cursor.getString(4);
+                            String sGender = cursor.getString(5);
+                            String sCity = cursor.getString(6);
+
+                            //Log.d("RESPONSE_DATA",sUserId+"\n"+sName+"\n"+sEmail+"\n"+sContact+"\n"+sPassword+"\n"+sGender+"\n"+sCity);
+
+                            sp.edit().putString(ConstantSp.USERID,sUserId).commit();
+                            sp.edit().putString(ConstantSp.NAME,sName).commit();
+                            sp.edit().putString(ConstantSp.EMAIL,sEmail).commit();
+                            sp.edit().putString(ConstantSp.CONTACT,sContact).commit();
+                            sp.edit().putString(ConstantSp.PASSWORD,sPassword).commit();
+                            sp.edit().putString(ConstantSp.GENDER,sGender).commit();
+                            sp.edit().putString(ConstantSp.CITY,sCity).commit();
+
+                        }
+
                         System.out.println("Login Successfully");
                         Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
                         Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
